65a16cc4933eb7f4dffcc84c1e65b2efeecb259a
Daniel Hewes <daniel@lambdacreatives.com>
2016-04-11 14:47:22 -0700

    Format code.

--- web/wp-config.php
+++ web/wp-config.php
@@ -4,62 +4,53 @@ require __DIR__ . '/../phpdotenv/src/Dotenv.php';
 require __DIR__ . '/../phpdotenv/src/Loader.php';
 require __DIR__ . '/../phpdotenv/src/Validator.php';
 
-$dotenv = new Dotenv\Dotenv( dirname( __DIR__ ) );
+$dotenv = new Dotenv\Dotenv(dirname( __DIR__ ));
 $dotenv->load();
-$dotenv->required( array(
-  'DB_NAME',
-  'DB_USER',
-  'DB_PASSWORD',
-  'DB_HOST',
-  'WP_ENV',
-  'WP_HOME',
-  'WP_SITEURL'
-) );
-
+$dotenv->required(array('DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST', 'WP_ENV', 'WP_HOME', 'WP_SITEURL'));
 
 // ** MySQL settings ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $_ENV['DB_NAME'] );
+define('DB_NAME', $_ENV['DB_NAME']);
 
 /** MySQL database username */
-define( 'DB_USER', $_ENV['DB_USER'] );
+define('DB_USER', $_ENV['DB_USER']);
 
 /** MySQL database password */
-define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 
 /** MySQL hostname */
-define( 'DB_HOST', $_ENV['DB_HOST'] );
+define('DB_HOST', $_ENV['DB_HOST']);
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
-define('AUTH_KEY',         $_ENV['AUTH_KEY']);
-define('SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY']);
-define('LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY']);
-define('NONCE_KEY',        $_ENV['NONCE_KEY']);
-define('AUTH_SALT',        $_ENV['AUTH_SALT']);
+define('AUTH_KEY', $_ENV['AUTH_KEY']);
+define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
+define('NONCE_KEY', $_ENV['NONCE_KEY']);
+define('AUTH_SALT', $_ENV['AUTH_SALT']);
 define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
-define('LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT']);
-define('NONCE_SALT',       $_ENV['NONCE_SALT']);
+define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
+define('NONCE_SALT', $_ENV['NONCE_SALT']);
 
 $table_prefix = 'wp_';
 
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/app' );
-define( 'WP_CONTENT_URL', 'http://' . $_ENV['WP_SITEURL'] . '/app' );
-define( 'WPMU_PLUGIN_DIR', dirname( __FILE__ ) . '/app/mu-plugins' );
-define( 'WPMU_PLUGIN_URL', 'http://' . $_ENV['WP_SITEURL'] . '/app/mu-plugins' );
+define('WP_CONTENT_DIR', dirname( __FILE__ ) . '/app');
+define('WP_CONTENT_URL', $_ENV['WP_HOME'] . '/app');
+define('WPMU_PLUGIN_DIR', dirname( __FILE__ ) . '/app/mu-plugins');
+define('WPMU_PLUGIN_URL', $_ENV['WP_HOME'] . '/app/mu-plugins');
 
-define( 'WP_HOME', $_ENV['WP_HOME'] );
-define( 'WP_SITEURL', $_ENV['WP_SITEURL'] );
+define('WP_HOME', $_ENV['WP_HOME']);
+define('WP_SITEURL', $_ENV['WP_SITEURL']);
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+if ( ! defined('ABSPATH' ) ) {
+    define('ABSPATH', dirname( __FILE__ ) . '/');
 }
 
 /** Sets up WordPress vars and included files. */
